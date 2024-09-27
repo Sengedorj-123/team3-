70b1bef84b10e6b4b2f6f6f5e6f007111f887970
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2024-09-27 13:52:59 -0700

    fonwawesome added

---
+++ crud-project/back-end/bash-script/font-awesome.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+npm i --save @fortawesome/fontawesome-svg-core
+npm i --save @fortawesome/free-solid-svg-icons
+npm i --save @fortawesome/free-regular-svg-icons
+npm i --save @fortawesome/free-brands-svg-icons
+npm i --save @fortawesome/react-fontawesome@latest
